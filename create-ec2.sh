6b46652336d9abd27323e46a87fad4f09a7183bc
Fred W <112580756+frederickw082922@users.noreply.github.com>
2022-12-14 10:00:41 -0500

    Update create-ec2.sh

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,4 +4,6 @@
 # Author: Sumanth
 # What does this script do: It launches an ec2 instance in my aws account 
 
+# aws ec2 run-instances --image-id $ami --count $count --instance-type $typ --key-name $key --security-group-ids $sg --subnet-id $subnet
+
 aws ec2 run-instances --image-id ami-0e31b343ec333d397 --count 1 --instance-type t1.micro --key-name sumanthPublicKey --security-groups-ids sg-0b56f3cdd4c96118a --region us-east-1
